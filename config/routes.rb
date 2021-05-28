540d676592a6cd41f2aa1a2c7c19be1204348127
hiroyuki_yamashita <hirokore3221@gmail.com>
2021-05-28 15:33:21 +0900

    update: error1 blogs修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # blogコントローラー名が間違っている
-  # 正しいコントローラー名に修正
+  # エラー原因:blogコントローラー名が間違っている
+  # 修正の意図:正しいコントローラー名に修正
   resources :blogs do
     resources :comments, only: [:create, :destroy]
   end
