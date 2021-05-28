e8547f5a657a2aab29e48fd69031cfcc7449d68e
hiroyuki_yamashita <hirokore3221@gmail.com>
2021-05-28 15:49:59 +0900

    update: error2-3 _newとblogモデル修正

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,5 +1,7 @@
 class Blog < ApplicationRecord
-  has_many :comment
+  # エラー原因:アソシエーション名が慣習に沿ってない
+  # 修正の意図:慣習に合わせて、commentsに変更
+  has_many :comments
 
   validates :title, presence: true
   validates :content, presence: true
